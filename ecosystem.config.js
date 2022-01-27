14a6bd2aca9adaa954c957d74089a1115d13c473
Jakub Gumowski <jakgum1@st.amu.edu.pl>
2022-01-27 11:52:51 +0100

    pm2 conf fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,10 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
+      args: 'start',
+      watch: ['../'],
+      watch_delay: 1000,
+      ignore_watch: ['node_modules']
     }
   ]
 }
